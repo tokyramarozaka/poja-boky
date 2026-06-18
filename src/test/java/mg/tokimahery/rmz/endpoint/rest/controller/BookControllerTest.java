36bf15b224c3a242263b2e83d25f5eaafff93717
tokyramarozaka <tokyramarozaka@gmail.com>
2026-06-18 23:37:31 +0300

    format: all files

--- src/test/java/mg/tokimahery/rmz/endpoint/rest/controller/BookControllerTest.java
+++ src/test/java/mg/tokimahery/rmz/endpoint/rest/controller/BookControllerTest.java
@@ -1,6 +1,5 @@
 package mg.tokimahery.rmz.endpoint.rest.controller;
 
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
